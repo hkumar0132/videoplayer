d58f98a9e42ce142eeb66d3e59a5170650cc3b09
Himanshu Kumar <hkumar0132@gmail.com>
2021-04-10 19:13:08 +0530

    backend and frontend restart enabled on ctrl+s

--- client/server.js
+++ client/server.js
@@ -1,12 +1,44 @@
-const express = require("express");
-const path = require("path");
+const http = require('http');
+const fs = require('fs');
 
-const app = express();
+let html, css, js;
 
-app.use("/static", express.static(path.resolve(__dirname, "static")));
-
-app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "static", "index.html"));
+fs.readFile('./client/index.html', function (err, data) {
+  if (err) {
+    throw err;
+  }
+  html = data;
+});
+fs.readFile('./client/static/css/index.css', function (err, data) {
+  if (err) {
+    throw err;
+  }
+  css = data;
+});
+fs.readFile('./client/static/js/index.js', function (err, data) {
+  if (err) {
+    throw err;
+  }
+  js = data;
 });
 
-app.listen(process.env.PORT || 5060, () => console.log("Server running..."));
+http.createServer((req, res) => {
+  res.statusCode = 200;
+if(req.url.indexOf('.css') != -1){
+   res.writeHead(200, {'Content-Type': 'text/css'});
+   res.write(css);
+   res.end();
+   return;
+  }
+
+  if(req.url.indexOf('index.js') != -1){
+   res.writeHead(200, {'Content-Type': 'text/javascript'});
+   res.write(js);
+   res.end();
+   return;
+  }
+
+res.writeHeader(200, {"Content-Type": "text/html"});
+  res.write(html);
+  res.end();
+}).listen(3000);

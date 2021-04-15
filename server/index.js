b11e0c1ae3b51ea29c805e4469e131475c1dd97d
Himanshu Kumar <hkumar0132@gmail.com>
2021-04-15 18:42:03 +0530

    videoPost API tested

--- server/index.js
+++ server/index.js
@@ -2,61 +2,39 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
 const mongoose = require('mongoose');
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
-
-const mongoURI = require('./config/key');
-const checkAuth = require('./middleware/check-auth');
-
-async function listDatabases(client){
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
-
-async function main() {
-
-  const client = new MongoClient(mongoURI);
-
-  try {
-      // Connect to the MongoDB cluster
-      await client.connect();
-
-      // Make the appropriate DB calls
-      await  listDatabases(client);
-
-  } catch (e) {
-      console.error(e);
-  } finally {
-      await client.close();
-  }
-}
-
-main().catch(console.error);
 
 
 // Used to log everything like GET, POST, etc requests
 app.use(morgan('dev'));
-// It ensures that we prevent Cross-Origin Resource Sharing(CORS) errors
-// If client made req on localhost:4000, and received res from server which
-// has localhost:3000 req will fail. It is always the case with RESTful APIs
-// So, we attach headers from servers to client to tell browser that it's OK
 app.use(cors());
-// extended: true allows to parse extended body with rich data in it
-// We will use false only allows simple bodies for urlencoded data
 app.use(bodyParser.urlencoded({extended: false}));
-// Extracts json data and makes it easy readable to us
 app.use(bodyParser.json());
 
-// To make uploads folder publically available with '/api/videos' route
-app.use('/api/videos', express.static('media/uploads'));
+const mongoURI = require('./config/key');
 
-// Routes
-// app.use('/api/signIn', require('./routes/Authentication/signIn'));
-// app.use('/api/register', require('./routes/Authentication/register'));
-app.use('/api/videoGet', require('./routes/Video/videoGet'));
-app.use('/api/videoPost', require('./routes/Video/videoPost'));
+require('dotenv').config({ path: 'ENV_FILENAME' });
+
+const connect = mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// @TODO -> app.use('/users', require('./routes/user.js'));
+app.use('/video', require('./routes/video.js'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server Listening on ${port}`);
+});
 
 module.exports = app;

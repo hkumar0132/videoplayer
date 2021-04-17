8ea2887c65493fe5c50d4d298ac223419e7c2ff1
ascyrax <ascyrax01@gmail.com>
2021-04-17 13:07:17 +0530

    top navigation section added. search bar . top right nav button from youtube cloned. search button temporarily replaced by a link with ion-icon

---
+++ client/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<link rel="stylesheet" type="text/css" href="vendors/css/normalize.css">
+<link rel="stylesheet" type="text/css" href="resources/css/style.css">
+<link href='https://fonts.googleapis.com/css?family=Lato:100,300,400,300italic' rel='stylesheet' type='text/css'>
+<title>MeTube</title>
+<meta charset="utf-8">
+    
+</head>
+
+<body>
+
+
+
+  <script src="source/algo.js"></script>
+
+</body>
+</html> 
---
+++ client/server/config/key.js
@@ -0,0 +1,2 @@
+const mongoURI = `mongodb+srv://Himanshu:6WvDrQcpyeqAa3GM@cluster0.qjx6u.mongodb.net/test`;
+module.exports = mongoURI;
